d3e97cd09700a206ef11d0e4e6c9346d03424fa3
Yasmina IDJA <yidja@nrj.fr>
2021-07-03 16:21:48 +0200

    [yidja](feature): footer

--- src/composants/Accueil/Footer.tsx
+++ src/composants/Accueil/Footer.tsx
@@ -2,22 +2,22 @@ import React from 'react'
 
 function Footer() {
     return (
-    <div>
-        <div className="foot">
-         <footer className="bg-light text-center text-lg-start">
-          <div className="container p-4">
-            <div className="row">
-            <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-            <a href="#!" className="text">Mentions légales</a>
-            <a href="#!" className="text">Gestion des données persnnelles et Cookies </a>
-            <a href="#!" className="text">Conditions générales d'utilisation</a>
-            <a href="#!" className="text">Conditions générales de vente</a>
-        </div>
-        </div>
-        </div>
-        </footer>
+    
+    <div id="footer">
+        <footer >
+          <div id="liens">
+             <a href="#!" className="text">Mentions légales</a>
+             <a href="#!" className="text">Gestion des données personnelles et Cookies </a>
+             <a href="#!" className="text">Conditions générales d'utilisation</a>
+             <a href="#!" className="text">Conditions générales de vente</a>
+             <a href="#!" className="text">Nous contacter</a> 
           </div>
-        </div>
+          <div className="text-center p-3">
+          © 2021 Copyright: 
+            <a className="text"> Greenand</a>
+          </div>
+        </footer>
+    </div>
     )
 }
 
