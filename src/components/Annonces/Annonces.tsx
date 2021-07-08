e74c3f4793076e527d1f8da7281f9b5c8154b3af
Tiziano Ghisotti <tiziano.ghisotti@gmail.com>
2021-07-08 11:32:37 +0200

    [tghisotti] (feature) new front

--- src/components/Annonces/Annonces.tsx
+++ src/components/Annonces/Annonces.tsx
@@ -1,159 +1,76 @@
+//@ts-nocheck
+import React, { useEffect, useState } from "react";
+
 import "../../styles/style_Annonce.css";
+import { getEvenements } from "../../adapters/EvenementManagement/evenementAdapter";
+import { Link } from "react-router-dom";
 
-function annonces() {
-    return (
-        <div className="container-fluid">
-            <hr className="mb-5" />
-            <div className="row">
-                <div className=" col-lg-12 col-md-12 col-sm-12" id="chiffres">
-                    <div className="text-center">
-                        <div>
-                            <h3 className="section-heading text-uppercase" id="text"><b>Voici les offres disponibles</b></h3>
-                        </div>
-                    </div>
-                </div>
+function Annonces() {
+  const [activities, setActivities] = useState(``);
+
+  const images = [
+    "https://mdbootstrap.com/img/new/standard/city/040.jpg",
+    "https://mdbootstrap.com/img/new/standard/city/041.jpg",
+    "https://mdbootstrap.com/img/new/standard/city/042.jpg",
+    "https://mdbootstrap.com/img/new/standard/city/043.jpg",
+  ];
+
+  useEffect(() => {
+    const fetchActivities = async () => {
+      const response = await getEvenements();
+      setActivities(response.data);
+    };
+    fetchActivities();
+  }, []);
+  return (
+    <div className="container-fluid">
+      <hr className="mb-5" />
+      <div className="row">
+        <div className=" col-lg-12 col-md-12 col-sm-12" id="chiffres">
+          <div className="text-center">
+            <div>
+              <h3 className="section-heading text-uppercase" id="text">
+                <b>Voici les offres disponibles</b>
+              </h3>
             </div>
-
-            <div className="row py-5">
-
-            </div>
-
-
-            <div className="row">
-
-                <div className="col-md-3">
-                    <div className="card mb-2">
-                        <img className="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(60).jpg"
-                            alt="Card image cap" />
-                        <div className="card-body">
-                            <h4 className="card-title">Card title</h4>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the
-                                card's content.</p>
-                            <a href="/annonce" className="text-success px-3">
-                                <h6>Découvrir l'annonce <i className="fas fa-angle-double-right"></i></h6>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="col-md-3 clearfix d-none d-md-block">
-                    <div className="card mb-2">
-                        <img className="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(47).jpg"
-                            alt="Card image cap" />
-                        <div className="card-body">
-                            <h4 className="card-title">Card title</h4>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the
-                                card's content.</p>
-                            <a href="/annonce" className="text-success px-3">
-                                <h6>Découvrir l'annonce <i className="fas fa-angle-double-right"></i></h6>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="col-md-3 clearfix d-none d-md-block">
-                    <div className="card mb-2">
-                        <img className="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(48).jpg"
-                            alt="Card image cap" />
-                        <div className="card-body">
-                            <h4 className="card-title">Card title</h4>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the
-                                card's content.</p>
-                            <a href="/annonce" className="text-success px-3">
-                                <h6>Découvrir l'annonce <i className="fas fa-angle-double-right"></i></h6>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="col-md-3 clearfix d-none d-md-block">
-                    <div className="card mb-2">
-                        <img className="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(48).jpg"
-                            alt="Card image cap" />
-                        <div className="card-body">
-                            <h4 className="card-title">Card title</h4>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the
-                                card's content.</p>
-                            <a href="/annonce" className="text-success px-3">
-                                <h6>Découvrir l'annonce <i className="fas fa-angle-double-right"></i></h6>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-
-            <div className="row">
-
-                <div className="col-md-3">
-                    <div className="card mb-2">
-                        <img className="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(60).jpg"
-                            alt="Card image cap" />
-                        <div className="card-body">
-                            <h4 className="card-title">Card title</h4>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the
-                                card's content.</p>
-                            <a href="/annonce" className="text-success px-3">
-                                <h6>Découvrir l'annonce <i className="fas fa-angle-double-right"></i></h6>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="col-md-3 clearfix d-none d-md-block">
-                    <div className="card mb-2">
-                        <img className="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(47).jpg"
-                            alt="Card image cap" />
-                        <div className="card-body">
-                            <h4 className="card-title">Card title</h4>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the
-                                card's content.</p>
-                            <a href="/annonce" className="text-success px-3">
-                                <h6>Découvrir l'annonce <i className="fas fa-angle-double-right"></i></h6>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="col-md-3 clearfix d-none d-md-block">
-                    <div className="card mb-2">
-                        <img className="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(48).jpg"
-                            alt="Card image cap" />
-                        <div className="card-body">
-                            <h4 className="card-title">Card title</h4>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the
-                                card's content.</p>
-                            <a href="/annonce" className="text-success px-3">
-                                <h6>Découvrir l'annonce <i className="fas fa-angle-double-right"></i></h6>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="col-md-3 clearfix d-none d-md-block">
-                    <div className="card mb-2">
-                        <img className="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(48).jpg"
-                            alt="Card image cap" />
-                        <div className="card-body">
-                            <h4 className="card-title">Card title</h4>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the
-                                card's content.</p>
-                            <a href="/annonce" className="text-success px-3">
-                                <h6>Découvrir l'annonce <i className="fas fa-angle-double-right"></i></h6>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-
-            <div className="row">
-
-
-
-            </div>
+          </div>
         </div>
-    );
+      </div>
+
+      <div className="row py-5"></div>
+
+      <div className="row">
+        <div className="col-md-3 clearfix d-none d-md-block">
+          {activities &&
+            activities.map((activity, id) => (
+              <div key={activity._id}>
+                <div className="card mb-2">
+                  <img
+                    className="card-img-top"
+                    src={images[Math.floor(Math.random() * 4)]}
+                    alt="Card image cap"
+                  />
+                  <div className="card-body">
+                    <h4 className="card-title"> {activity.label}</h4>
+                    <p className="card-text">{activity.description}</p>
+                    <Link
+                      to={`/annonce/${activity._id}`}
+                      className="text-success px-3"
+                    >
+                      {" "}
+                      <h6>
+                        Découvrir l'annonce{" "}
+                        <i className="fas fa-angle-double-right"></i>
+                      </h6>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default annonces;
+export default Annonces;
--- src/components/Annonces/Form_Create_Annonce.tsx
+++ src/components/Annonces/Form_Create_Annonce.tsx
@@ -1,66 +1,192 @@
+//@ts-nocheck
 import "../../styles/style_Annonce.css";
 import forest from "../../assets/forest.jpg";
 import logo from "../../assets/Logo_text.png";
+import DatePicker from "react-multi-date-picker";
+import DatePanel from "react-multi-date-picker/plugins/date_panel";
+import TimePicker from "react-multi-date-picker/plugins/time_picker";
+import { useState, useEffect } from "react";
+import { getLands } from "../../adapters/LandManagement/landAdapter";
+import { createEvenement } from "../../adapters/EvenementManagement/evenementAdapter";
+import classnames from "classnames";
 
-function createAnnonces() {
-    return (
-        <div className="formCreate" id="formCreateAnnonceId">
-            <section className="form my-4 mx-5">
-                <div className="container">
-                    <div className="row py-5 d-flex flex-wrap align-items-center rowCreateAnnonce">
-                        <div className="col-lg-5 col-md">
-                            <img src={forest} className="img-fluid" alt="imgForest" />
-                        </div>
+function CreateAnnonces() {
+  const [values, setValues] = useState([]);
+  const [label, setLabel] = useState(``);
+  const [description, setDescription] = useState(``);
+  const [lands, setLands] = useState([]);
+  const [id, setId] = useState(``);
 
+  const [labelErr, setLabelErr] = useState(``);
+  const [descriptionErr, setDescriptionErr] = useState(``);
 
-                        <div className="col-lg-7 px-5 formCreateAnnonce">
-                            <img src={logo} className="img-fluid my-3" alt="" />
-                            <h4>Création d'une annonce</h4>
-                            <form >
-                                <div className="form-row">
-                                    <div className="col-lg-7">
-                                        <input type="text" placeholder="Intitulé de l'annonce" className="form-control rounded-pill my-3 p-4" />
-                                    </div>
-                                </div>
+  useEffect(() => {
+    const fetchLands = async () => {
+      const response = await getLands();
+      setLands(response.data);
+    };
+    fetchLands();
+  }, []);
 
-                                <div className="form-row">
-                                    <div className="col-lg-7">
-                                        <select className="form-control rounded-pill my-3 p-4" id="exampleFormControlSelect1" aria-label="Default select example">
-                                            <option>Choisissez...</option>
-                                            <option>Toile de Fontaine : 5 rue Boulevard la ville</option>
-                                            <option>Montaigne de Montmartre : 9 Square des Varennes</option>
-                                            <option>Retour de champ : Val de Marne</option>
-                                            <option>Rentrée de jardin : 12 bis Laurence</option>
-                                        </select>
-                                    </div>
-                                </div>
+  const handleChange = (e, name) => {
+    const evenement = {};
 
-                                <div className="form-row">
-                                    <div className="col-lg-7">
-                                        <input className="form-control rounded-pill my-3 p-4" id="dateinput" type="Date" required />
-                                    </div>
-                                </div>
+    evenement[name] = e.target.value;
 
-                                <div className="form-row">
-                                    <div className="col-lg-7">
-                                        <textarea className="form-control rounded my-3 p-4" placeholder="Description" id="exampleTextarea" required></textarea>
-                                    </div>
-                                </div>
+    switch (name) {
+      case `label`:
+        setLabel(evenement.label);
+        if (evenement.label.length < 3) {
+          setLabelErr("Le label nécessite 3 caractères au minimum");
+        } else if (evenement.label.length > 100) {
+          setLabelErr("Le label ne doit pas dépasser 100 caractères ");
+        } else {
+          setLabelErr("");
+        }
+        break;
 
+      case `description`:
+        console.log("ici")
+        setDescription(evenement.description);
+        if (evenement.description.length < 20) {
+          setDescriptionErr(
+            "La description nécessite 20 caractères au minimum"
+          );
+        } else {
+          setDescriptionErr("");
+        }
+        break;
+      case `selectLand`:
+        setId(evenement.selectLand);
+        break;
+    }
+  };
 
-                                <div className="form-row">
-                                    <div className="col-lg-7">
-                                        <button type="button" className="btn1 mt-3 mb-5">Valider</button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
+  const handleCreateEvenement = async (e) => {
+    e.preventDefault();
+
+    if (label && description && !labelErr && !descriptionErr) {
+      const idLand = id;
+      let dayBody = new Array();
+
+      for (var i = 0; i < values.length; i++) {
+        dayBody.push({
+          startOfDay: values.toString().split(",")[i].toString().split(" ")[0],
+          startOfHour: values.toString().split(",")[i].toString().split(" ")[1],
+        });
+      }
+
+      console.log(idLand);
+      await createEvenement(idLand, {
+        label,
+        description,
+        disponibility: dayBody,
+      });
+    }
+  };
+
+  return (
+    <div className="formCreate" id="formCreateAnnonceId">
+      <section className="form my-4 mx-5">
+        <div className="container">
+          <div className="row py-5 d-flex flex-wrap align-items-center rowCreateAnnonce">
+            <div className="col-lg-5 col-md">
+              <img src={forest} className="img-fluid" alt="imgForest" />
+            </div>
+
+            <div className="col-lg-7 px-5 formCreateAnnonce">
+              <img src={logo} className="img-fluid my-3" alt="" />
+              <h4>Création d'une annonce</h4>
+              <form onSubmit={handleCreateEvenement}>
+                <div className="form-row">
+                  <div className="col-lg-7">
+                    <input
+                      type="text"
+                      name="label"
+                      className={classnames(
+                        "form-control rounded-pill my-3 p-4",
+                        {
+                          "is-invalid": labelErr,
+                          "is-valid": !labelErr && label.length,
+                        }
+                      )}
+                      onChange={(e) => handleChange(e, `label`)}
+                      placeholder="Intitulé de l'annonce"
+                    />
+                    {labelErr && (
+                      <small className="text-danger">{labelErr}</small>
+                    )}
+                  </div>
                 </div>
-            </section>
-        </div>
 
-    );
+                <div className="form-row">
+                  <div className="col-lg-7">
+                    <select
+                      className="form-control rounded-pill my-3"
+                      id="selectLand"
+                      required
+                      onChange={(e) => handleChange(e, `selectLand`)}
+                    >
+                      <option>Faites votre choix..</option>
+                      {lands.map((land) => (
+                        <option key={land._id} value={land._id}>
+                          {land.city} - {land.street}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                </div>
+
+                <div className="form-row">
+                  <DatePicker
+                    id="creneauxID"
+                    value={values}
+                    multiple
+                    type="icon"
+                    className="form-label mt-4"
+                    sort
+                    onChange={setValues}
+                    format="DD/MM/YYYY HH:mm"
+                    plugins={[
+                      <TimePicker position="bottom" hideSeconds />,
+                      <DatePanel />,
+                    ]}
+                  />
+                </div>
+
+                <div className="form-row">
+                  <div className="col-lg-7">
+                    <textarea
+                      name="description"
+                      className={classnames("form-control rounded my-3 p-4", {
+                        "is-invalid": descriptionErr,
+                        "is-valid": !descriptionErr && description.length,
+                      })}
+                      placeholder="Description"
+                      id="exampleTextarea"
+                      onChange={(e) => handleChange(e, `description`)}
+                      required
+                    ></textarea>
+                    {descriptionErr && (
+                      <small className="text-danger">{descriptionErr}</small>
+                    )}
+                  </div>
+                </div>
+
+                <div className="form-row">
+                  <div className="col-lg-7">
+                    <button type="submit" className="btn1 mt-3 mb-5">
+                      Valider
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
 
-export default createAnnonces;
+export default CreateAnnonces;
